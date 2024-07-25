841bb179e3a2968d4b451d8c09518f03ce2b46fe
Kevin harrington <mad.hephaestus@gmail.com>
2024-07-25 14:05:51 -0400

    match internal api

--- resize.groovy
+++ resize.groovy
@@ -7,12 +7,14 @@ CSG starting = new Cube(20).toCSG().toZMin().setColor(Color.RED)
 				.movez(30)
 
 TransformNR height = new TransformNR(0,0,40)
+TransformNR rightFront = new TransformNR(40,10,0)
+TransformNR leftRear = new TransformNR(-10,80,0)
+
 
 CSG resizeUp = starting.scaleToMeasurmentZ(height.getZ())
 				.toZMin()
 				.movez(starting.getMinZ())
-TransformNR rightFront = new TransformNR(40,10,0)
-TransformNR leftRear = new TransformNR(-10,80,0)
+
 
 double xdimen = rightFront.getX()-leftRear.getX()
 double ydimen = rightFront.getY()-leftRear.getY()
