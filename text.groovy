4afe2f5b64ce665c18f7609ed2357466d1a4ffad
Kevin harrington <mad.hephaestus@gmail.com>
2024-08-25 14:54:27 -0400

    Adding text to the basic shapes

---
+++ text.groovy
@@ -0,0 +1,23 @@
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.Cube
+import javafx.scene.paint.Color
+
+import eu.mihosoft.vrl.v3d.parametrics.*;
+CSG getObject(){
+
+	StringParameter word = new StringParameter(	"CaDoodle_TextGeneration_Value",
+										"TEXT",[])
+	StringParameter font = new StringParameter(	"CaDoodle_TextGeneration_Font",
+		"Arial",["Arial","System",
+"Serif",
+"SansSerif",
+"Monospaced"])
+	CSG text = CSG.text( word.getStrValue(),  20,  20, font.getStrValue())
+	
+	text=text.toZMin().setColor(Color.RED)
+	return text
+		.setParameter(word)
+		.setParameter(font)
+		.setRegenerate({getObject()})
+}
+return getObject()
